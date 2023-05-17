2d395f3f08cdccef5274d34e55cb3095076030b3
Rajee Chezhiyan <rajeswarichezhiyan01@gmail.com>
2023-05-16 20:58:48 -0400

    Finished till updateguess

--- wordle.go
+++ wordle.go
@@ -1,1 +1,105 @@
 package wordle
+
+import (
+	"strings"
+)
+
+const (
+	maxGuesses = 6
+	wordSize   = 5
+)
+
+// letterStatus can be none, correct, present, or absent
+type letterStatus int
+
+const (
+	// none = no status, not guessed yet
+	none letterStatus = iota
+	// absent = not in the word
+	absent
+	// present = in the word, but not in the correct position
+	present
+	// correct = in the correct position
+	correct
+)
+
+type wordleState struct {
+	// word is the word that the user is trying to guess
+	word [wordSize]byte
+	// guesses holds the guesses that the user has made
+	guesses [maxGuesses]guess
+	// currGuess is the index of the available slot in guesses
+	currGuess int
+}
+
+// guess is an attempt to guess the word
+type guess [wordSize]letter
+
+type letter struct {
+	// char is the letter that this struct represents
+	char byte
+	// status is the state of the letter (absent, present, correct)
+	status letterStatus
+}
+
+func newWordleState(word string) wordleState {
+
+	w := wordleState{}
+	copy(w.word[:], word)
+	return w
+}
+
+func newLetter(b byte) letter {
+	lt := letter{
+		char:   b,
+		status: 0,
+	}
+
+	return lt
+
+}
+
+func newGuess(gw string) guess {
+	g := guess{}
+
+	for i, c := range gw {
+		g[i] = newLetter(byte(c))
+	}
+
+	return g
+}
+
+func (g *guess) string() string {
+	str := ""
+	for _, l := range g {
+		if 'A' <= l.char && l.char <= 'Z' {
+			str += string(l.char)
+		}
+	}
+	return str
+}
+
+// updateLettersWithWord updates the status of the letters in the guess based on a word
+func (g *guess) updateLettersWithWord(word [wordSize]byte) {
+
+	for i := range g {
+		l := &g[i]
+		if l.char == word[i] {
+			l.status = correct
+		} else if strings.Contains(string(word[:]), string(l.char)) {
+			l.status = present
+		} else {
+			l.status = absent
+		}
+	}
+}
+
+// appendGuess adds a guess to the wordleState. It returns an error
+// if the guess is invalid.
+func (w *wordleState) appendGuess(g guess) error {
+
+	guesses := &w.guesses
+
+	guesses.append(g)
+
+}
--- wordle_test.go
+++ wordle_test.go
@@ -1,1 +1,94 @@
 package wordle
+
+import "testing"
+
+func TestNewWordleState(t *testing.T) {
+
+	word := "HELLO"
+	ws := newWordleState(word)
+	wordleAsString := string(ws.word[:])
+
+	t.Log("Created wordleState:")
+	t.Logf("		word: %s", wordleAsString)
+	t.Logf("		guesses: %v", ws.guesses)
+	t.Logf("		currGuess: %v", ws.currGuess)
+
+	if wordleAsString != word {
+		t.Errorf("word = %s; want %s", wordleAsString, word)
+	}
+
+}
+
+func statusToString(status letterStatus) string {
+	switch status {
+	case none:
+		return "none"
+	case correct:
+		return "correct"
+	case present:
+		return "present"
+	case absent:
+		return "absent"
+	default:
+		return "unknown"
+	}
+}
+func TestNewGuess(t *testing.T) {
+
+	guess := "LOVER"
+
+	wg := newGuess(guess)
+
+	t.Logf("Newguess: %v", wg.string())
+
+	for i, l := range wg {
+
+		if l.char != guess[i] || l.status != none {
+
+			t.Errorf(
+				"letter[%d] = %c, %s; want %c, none",
+				i,
+				l.char,
+				statusToString(l.status),
+				guess[i],
+			)
+		}
+
+	}
+
+}
+
+func TestUpdateLettersWithWord(t *testing.T) {
+	guessWord := "YIELD"
+	guess := newGuess(guessWord)
+
+	var word [wordSize]byte
+	copy(word[:], "HELLO")
+	guess.updateLettersWithWord(word)
+
+	statuses := []letterStatus{
+		absent,  // "Y" is not in "HELLO"
+		absent,  // "I" is not in "HELLO"
+		present, // "E" is in "HELLO" but not in the correct position
+		correct, // "L" is in "HELLO" and in the correct position
+		absent,  // "D" is not in "HELLO"
+	}
+
+	// Check that statuses are correct
+
+	for i, l := range guess {
+
+		if l.status != statuses[i] {
+			t.Errorf(
+				"letter[%d] = %c, %s; want %c, %s",
+				i,
+				l.char,
+				statusToString(l.status),
+				guessWord[i],
+				statusToString(statuses[i]),
+			)
+		}
+
+	}
+
+}
